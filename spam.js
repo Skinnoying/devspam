88ba83fe29e20a2558c6ddc4f99fe25b6390dbe1
Skinno <140928925+Skinnoying@users.noreply.github.com>
2024-08-23 19:47:06 +0700

    Update code spam.js

--- spam.js
+++ spam.js
@@ -1,37 +1,37 @@
-document.addEventListener('click', () => {
+document.addEventListener('click', async () => {
     let msgBox = document.querySelectorAll("[contenteditable='true']")[1];
-    let msg = msgBox.innerText;
     let count = 1;
 
     // Prompt for Message
     let pesan = prompt('Input Message:');
-    let jumlah = parseInt(prompt('Number of Message:'));
+    let jumlah = parseInt(prompt('Number of Messages:'));
 
     if (pesan && !isNaN(jumlah)) {
-        (async () => {
-            while (count <= jumlah) {
-                await new Promise((resolve) => {
-                    msgBox.dispatchEvent(new InputEvent("beforeinput", {
-                        inputType: "insertText",
-                        data: pesan,
-                        bubbles: true,
-                        cancelable: true
-                    }));
-                    resolve();
-                });
-                msgBox.dispatchEvent(
-                    new KeyboardEvent('keydown', {
-                        key: "Enter",
-                        code: "Enter",
-                        which: 13,
-                        keyCode: 13,
-                        bubbles: true,
-                        view: window
-                    })
-                )
-                await new Promise(resolve => setTimeout(resolve, 10));
-                count++;
-            }
-        })();
+        while (count <= jumlah) {
+            // Insert the message into the msgBox
+            msgBox.innerText = pesan;
+
+            // Trigger the input event
+            msgBox.dispatchEvent(new InputEvent("input", {
+                inputType: "insertText",
+                data: pesan,
+                bubbles: true,
+                cancelable: true
+            }));
+
+            // Simulate pressing 'Enter'
+            msgBox.dispatchEvent(new KeyboardEvent('keydown', {
+                key: "Enter",
+                code: "Enter",
+                which: 13,
+                keyCode: 13,
+                bubbles: true
+            }));
+
+            // Wait for a short interval
+            await new Promise(resolve => setTimeout(resolve, 10));
+
+            count++;
+        }
     }
 });
